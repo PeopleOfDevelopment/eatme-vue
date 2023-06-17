891c8c473f90473c950950447190876b098d3f73
alsgud2659 <alsgud2659@naver.com>
2023-06-17 22:55:56 +0900

    0617 Create Organization Repository

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+
+// 타입스크립트 연결 확인
+const testStr: string = "연결 완료"
+console.log("타입스크립트 연결 확인 : " + testStr)
+
+const check = getDbConnectionStatus()
+
+console.log(check)
+
+/** DB 연결확인  */
+async function getDbConnectionStatus() {
+  const result = await axios.post("/userlist/query")
+  return result
+}
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  // npm run build 타겟 설정 (백엔드 연결)
+  outputDir : "../eatme-server/src/main/resources/static",
+
+  // npm run server 개발 진행시 포트가 다르기 때문에 프록시 설정해줘야 함
+  // target : 백엔드 port가 들어감
+  // changeOrigin -> true로 해야 cors 문제 해결할 수 있음
+  devServer: {
+    proxy: {
+      '/': {
+        target: "http://localhost:7070",
+        changeOrigin: true,
+        ws: false
+      }
+    }
+  }
+})
