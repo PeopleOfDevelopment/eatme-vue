de4aa66cbe3427f101d031744335a7386b6293d4
kongkong2233 <dbqls1703@naver.com>
2023-07-09 14:04:20 +0900

    사이드바/모달창

---
+++ src/views/common/components/modal1.vue
@@ -0,0 +1,61 @@
+<template>
+    <button @click="modalOpen = true">모달창테스트</button>
+    <div class="black-bg" v-if="modalOpen == true">
+    <div class="modal1">
+        <p class="t1">정말 탈퇴하시겠어요?</p>
+        <p class="t2">탈퇴할 시, 계정은 삭제되며 복구되지 않습니다.</p>
+        <Buttons color="#D13125" text="탈퇴" fontColor="#fff" borderColor="solid 1px #D13125"
+            style="padding: 10px 40px 10px 40px; margin-right: 100px;"></Buttons>
+        <Buttons color="#DDE5DB" text="취소" fontColor="#717971" borderColor="solid 1px #DDE5DB"
+        style="padding: 10px 40px 10px 40px;" @click="modalOpen = false"></Buttons>
+    </div>
+    </div>
+</template>
+
+<script>
+import Buttons from './Btn.vue';
+
+    export default {
+        components: {
+            Buttons,
+        },
+        data() {
+            return {
+                modalOpen: false,
+            }
+        }
+    }
+</script>
+
+<style>
+.black-bg{
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.5);
+    position: fixed;
+    padding: 20px;
+}
+
+.modal1 {
+    width: 1000px;
+    height: 392px;
+    background-color: white;
+    border: solid 1px #DDE5DB;
+    border-radius: 10px;
+    margin: auto;
+    font-family: Pretendard;
+}
+
+.t1 {
+    color: #161D17;
+    padding-top: 40px;
+    font-weight: bold;
+    font-size: 34px;
+}
+
+.t2 {
+    color: #2B322C;
+    padding-bottom: 24px;
+    font-size: 20px;
+}
+</style>
---
+++ src/views/login/Join.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="eatMe">
+        <p class="text1">EAT ME</p>
+        <p class="text2">회원가입</p>
+        <div class="box1">
+            <p style="color: #2E312E">이미 EAT ME의 회원이신가요?</p>
+            <p>로그인</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+.eatMe {
+    margin-top: 5%;
+    display: block;
+    text-align: center;
+    padding: 0px;
+}
+
+.text1 {
+    font-size: 48px;
+    margin: 16px;
+}
+
+.text2 {
+    color: #2E312E;
+    font-size: 32px;
+    margin: 20px;
+}
+
+.box1 {
+    display: block;
+    text-align: center;
+    font-size: 0;
+}
+
+.box1 p {
+    display: inline-block;
+    font-size: 20px;
+    padding-right: 100px;
+    font-weight: lighter;
+    font-family: Pretendard;
+}
+</style>
