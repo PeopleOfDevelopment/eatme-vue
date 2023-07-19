e59101776fa1230ef5386b22b1b4c36ddc5126ab
kongkong2233 <dbqls1703@naver.com>
2023-07-19 23:06:39 +0900

    파일이름 수정

---
+++ src/views/common/components/PlaceHolder.vue
@@ -0,0 +1,31 @@
+<template>
+    <input class="inputInfor" type="text" :placeholder="placeholder">
+</template> 
+
+<script>
+    export default {
+        props: {
+            placeholder: {
+                type: String,
+                required: true, 
+            }
+        }
+    };
+</script>
+
+<style scoped>
+.inputInfor {
+    width: 92%;
+    padding: 18px;
+    margin: 7px;
+    font-size: 18px;
+    border-radius: 4px;
+    border: solid 1px #C1C9BF;
+    font-weight: bold;
+    font-family: Pretendard;
+}
+
+input::placeholder {
+    color: #A5ADA4;
+}
+</style>
