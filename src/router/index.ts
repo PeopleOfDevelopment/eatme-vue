4afe35ed3364c23d7b027ba25371b3e6b292ec08
kimcyan <116955327+kimcyan@users.noreply.github.com>
2023-07-19 18:40:58 +0900

    vue.config.js '/' -> '/api' 수정, 푸터, 사이드바 너비 변경

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,26 @@
-import { createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 //연결할 각 컴포넌트 import
-import Login from '../views/login/Login.vue'
-import Join from '../views/login/Join.vue'
-import Basket from '../views/purchase/basket/Basket.vue'
-import PurchaseHistory from '../views/purchase/purchase-history/PurchaseHistory.vue'
-
-
+import Main from '../views/common/main/Main.vue';
+import Login from '../views/login/Login.vue';
+import Join from '../views/login/Join.vue';
+import Basket from '../views/purchase/basket/Basket.vue';
+import PurchaseHistory from '../views/purchase/purchase-history/PurchaseHistory.vue';
 
 //라우터설계(경로랑 컴포넌트 배열로 입력)
 const routes = [
-    { path: '/', component: Basket }, //컴포넌트값에 현재 보고싶은 화면 컴포넌트 넣으면 됩니다..
-]
-
+  { path: '/', component: Main }, //컴포넌트값에 현재 보고싶은 화면 컴포넌트 넣으면 됩니다..
+  { path: '/login', component: Login },
+  { path: '/join', component: Join },
+  { path: '/basket', component: Basket },
+  { path: '/purchase-history', component: PurchaseHistory },
+];
 
 //라우터생성
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 //라우터 추출
-export {router}
+export { router };
