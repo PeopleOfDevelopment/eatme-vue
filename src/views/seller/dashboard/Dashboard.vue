c0a030d1bc738215f16453e2117d8d7cb0e859e1
kimcyan <116955327+kimcyan@users.noreply.github.com>
2023-08-20 13:31:25 +0900

    Dashboard 카드 내부 추가

--- src/views/seller/dashboard/Dashboard.vue
+++ src/views/seller/dashboard/Dashboard.vue
@@ -19,21 +19,54 @@
             <span class="dash-title">픽업예정</span>
             <span class="dash-count">2</span>
           </div>
-          <div class="dash-contents-container"></div>
+          <div class="dash-contents-container">
+            <div v-for="item in pickupList" class="cs-tr">
+              <div class="cs-img"></div>
+              <div class="cs-info-container">
+                <div class="cs-title">
+                  {{ item.name }} {{ item.quantity }}개
+                </div>
+                <div class="cs-detail-wrap">
+                  <div class="cs-detail">
+                    <div class="detail-label">구매자</div>
+                    <div class="detail-text">{{ item.buyer }}</div>
+                  </div>
+                  <div class="cs-detail">
+                    <div class="detail-label">결제일시</div>
+                    <div class="detail-text">{{ item.paymentTime }}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="cs-condition">{{ item.pickupTime }}</div>
+            </div>
+          </div>
         </div>
         <div class="dash-card">
           <div class="dash-title-container">
             <span class="dash-title">배송요청</span>
             <span class="dash-count">3</span>
           </div>
-          <div class="dash-contents-container"></div>
-        </div>
-        <div class="dash-card">
-          <div class="dash-title-container">
-            <span class="dash-title">문의</span>
-            <span class="dash-count">3</span>
+          <div class="dash-contents-container">
+            <div v-for="item in deliveryList" class="cs-tr">
+              <div class="cs-img"></div>
+              <div class="cs-info-container">
+                <div class="cs-title">
+                  {{ item.name }} {{ item.quantity }}개
+                </div>
+                <div class="cs-detail-wrap">
+                  <div class="cs-detail">
+                    <div class="detail-label">구매자</div>
+                    <div class="detail-text">{{ item.buyer }}</div>
+                  </div>
+                  <div class="cs-detail">
+                    <div class="detail-label">결제일시</div>
+                    <div class="detail-text">{{ item.paymentTime }}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="cs-condition">{{ item.condition }}</div>
+            </div>
           </div>
-          <div class="dash-contents-container"></div>
         </div>
       </div>
       <div class="dash-section">
@@ -61,9 +94,59 @@
         </div>
         <div class="dash-card">
           <div class="dash-title-container">
+            <span class="dash-title">문의</span>
+            <span class="dash-count">3</span>
+          </div>
+          <div class="dash-contents-container">
+            <div v-for="item in contactList" class="cs-tr">
+              <div class="cs-info-container">
+                <div class="cs-title">
+                  {{ item.title }}
+                </div>
+                <div class="cs-detail-wrap">
+                  <div class="cs-detail">
+                    <div class="detail-label">작성자</div>
+                    <div class="detail-text">{{ item.customer }}</div>
+                  </div>
+                  <div class="cs-detail">
+                    <div class="detail-label">작성일</div>
+                    <div class="detail-text">{{ item.postTime }}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="cs-condition">{{ item.condition }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="dash-section">
+        <div class="dash-card">
+          <div class="dash-title-container">
             <span class="dash-title">할인품목 추천</span>
           </div>
-          <div class="dash-contents-container"></div>
+          <div class="dash-contents-container table-card">
+            <div v-for="(item, index) in recommendList" class="cs-tr">
+              <div class="table-number">{{ index + 1 }}</div>
+              <div class="cs-img"></div>
+              <div class="cs-info-container">
+                <div class="cs-title">
+                  {{ item.name }}
+                </div>
+                <div class="cs-detail-wrap">
+                  <div class="cs-detail">
+                    <div class="detail-label">원가</div>
+                    <div class="detail-text">{{ item.price }} 원</div>
+                  </div>
+                </div>
+              </div>
+              <div class="cs-td discount-rate">
+                {{ item.recommendDiscount * 100 }}%
+              </div>
+              <div class="cs-td total-price">
+                {{ item.price * (1 - item.recommendDiscount) }} 원
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="dash-section">
@@ -71,13 +154,42 @@
           <div class="dash-title-container">
             <span class="dash-title">많이 판매된 제품</span>
           </div>
-          <div class="dash-contents-container"></div>
+          <div class="dash-contents-container table-card">
+            <div v-for="(item, index) in bestList" class="cs-tr">
+              <div class="table-number">{{ index + 1 }}</div>
+              <div class="cs-img"></div>
+              <div class="cs-info-container">
+                <div class="cs-title">
+                  {{ item.name }}
+                </div>
+                <div class="cs-detail-wrap">
+                  <div class="cs-detail">
+                    <div class="detail-label">원가</div>
+                    <div class="detail-text">{{ item.price }} 원</div>
+                  </div>
+                  <div class="cs-detail">
+                    <div class="detail-label">평균할인</div>
+                    <div class="detail-text discount-rate">
+                      {{ item.averageDiscount * 100 }}%
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="dash-card">
           <div class="dash-title-container">
             <span class="dash-title">공지사항</span>
           </div>
-          <div class="dash-contents-container"></div>
+          <div class="dash-contents-container table-card">
+            <div v-for="item in noticeList" class="cs-tr">
+              <div class="cs-title">
+                {{ item.title }}
+              </div>
+              <div class="cs-detail-wrap">{{ item.noticeDate }}</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -86,6 +198,89 @@
 <script setup>
 import Sidebar from '../../common/main/sidebar/Sidebar.vue';
 import Btn from '../../common/components/Btn.vue';
+import { ref } from 'vue';
+
+const pickupList = ref([
+  {
+    name: '[피그인더가든] 그린믹스 콜라겐 샐러드키트 5봉',
+    quantity: 1,
+    buyer: '김수현',
+    paymentTime: '20:32',
+    pickupTime: '20:50',
+  },
+  {
+    name: '[비비고] 그린믹스 콜라겐 샐러드키트 5봉',
+    quantity: 1,
+    buyer: '김미현',
+    paymentTime: '20:35',
+    pickupTime: '20:55',
+  },
+]);
+
+const deliveryList = ref([
+  {
+    name: '맛있는 고기',
+    quantity: 2,
+    buyer: '김미소',
+    paymentTime: '20:45',
+    condition: '배송대기',
+  },
+  {
+    name: '[비비고] 그린믹스 콜라겐 샐러드키트 5봉',
+    quantity: 1,
+    buyer: '김이지',
+    paymentTime: '20:50',
+    condition: '배송대기',
+  },
+]);
+const contactList = ref([
+  {
+    title: '배송 언제 와요?',
+    customer: '김미소',
+    postTime: '7월 5일',
+    condition: '답변대기',
+  },
+  {
+    title: '배송 문의',
+    customer: '김이지',
+    postTime: '7월 23일',
+    condition: '답변대기',
+  },
+]);
+const recommendList = ref([
+  {
+    name: '[피그인더가든] 그린믹스 콜라겐 샐러드키트 5봉',
+    price: 8900,
+    recommendDiscount: 0.25,
+  },
+  {
+    name: '[피그인더가든] 그린믹스 콜라겐 샐러드키트 5봉',
+    price: 8900,
+    recommendDiscount: 0.25,
+  },
+]);
+const bestList = ref([
+  {
+    name: '[피그인더가든] 그린믹스 콜라겐 샐러드키트 5봉',
+    price: 8900,
+    averageDiscount: 0.25,
+  },
+  {
+    name: '[피그인더가든] 그린믹스 콜라겐 샐러드키트 5봉',
+    price: 8900,
+    averageDiscount: 0.25,
+  },
+]);
+const noticeList = ref([
+  {
+    title: '서비스 점검 안내 (2023년 5월 26일 새벽 23시 50분 ~ 00시 5분)',
+    noticeDate: '2023.05.25',
+  },
+  {
+    title: '제품 등록 안내',
+    noticeDate: '2023.05.25',
+  },
+]);
 </script>
 
 <style scoped>
@@ -103,6 +298,29 @@ import Btn from '../../common/components/Btn.vue';
   display: flex;
   gap: 12px;
 }
+/*상단 공지*/
+.notif-wrapper {
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  gap: 10px;
+  background-color: var(--gray-white);
+  border-bottom: 1px solid var(--ngray100);
+}
+.notif-title {
+  color: var(--ngray800);
+}
+.notif-date {
+  color: var(--ngray400);
+}
+.notif-wrapper .btn {
+  border-radius: 50%;
+  padding: 8px;
+}
+.notif-wrapper .material-symbols-rounded {
+  font-size: 20px;
+}
+
 /*카드*/
 .dash-card {
   background-color: var(--gray-white);
@@ -123,6 +341,46 @@ import Btn from '../../common/components/Btn.vue';
 }
 .dash-count {
   color: var(--secondary-def);
+}
+
+.cs-tr {
+  display: flex;
+  padding: 8px 10px;
+  gap: 16px;
+  font-size: 16px;
+}
+.cs-img {
+  width: 90px;
+  height: 90px;
+  background-color: var(--gray100);
+}
+.cs-info-container {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  text-align: left;
+  flex: 1 0 0;
+}
+.cs-title {
+  color: var(--ngray800);
+}
+.cs-detail-wrap {
+  color: var(--ngray400);
+  display: flex;
+  flex-direction: column;
+}
+.cs-detail {
+  display: flex;
+  gap: 8px;
+}
+.detail-label {
+  width: 60px;
+}
+.detail-text {
+  flex: 1 0 0;
+}
+.cs-condition {
+  color: var(--primary-def);
 }
 /*매출액 통계 카드*/
 .stat-info-wrapper {
@@ -145,27 +403,26 @@ import Btn from '../../common/components/Btn.vue';
   color: var(--primary-d3);
   font-size: 20px;
 }
-
-/*상단 공지*/
-.notif-wrapper {
-  display: flex;
+/*할인품목 추천*/
+.table-card .cs-tr {
   align-items: center;
-  padding: 12px;
-  gap: 10px;
-  background-color: var(--gray-white);
-  border-bottom: 1px solid var(--ngray100);
 }
-.notif-title {
-  color: var(--ngray800);
-}
-.notif-date {
+.table-number {
   color: var(--ngray400);
+  padding-inline: 8px;
 }
-.notif-wrapper .btn {
-  border-radius: 50%;
-  padding: 8px;
+.cs-td {
+  width: 16%;
+  max-width: 100px;
 }
-.notif-wrapper .material-symbols-rounded {
-  font-size: 20px;
+.discount-rate {
+  color: var(--secondary-def);
+  font-weight: 700;
+}
+.total-price {
+  font-weight: 700;
+}
+.table-card .cs-title {
+  text-align: left;
 }
 </style>
