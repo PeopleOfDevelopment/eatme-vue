93337f34abb3a0264721b20be040c0ac8d034be7
kongkong2233 <dbqls1703@naver.com>
2023-08-29 03:18:05 +0900

    바코드 등록 리스트 페이지 1차

--- src/views/seller/barcode-reg/BarcodeRegList.vue
+++ src/views/seller/barcode-reg/BarcodeRegList.vue
@@ -1,48 +1,56 @@
 <template>
  <Sidebar isSeller="true"></Sidebar>
  <div id="main-wrapper">
-    <div class="top-box">
-        <div class="top-box-items">
-            <span class="material-symbols-rounded">arrow_back_ios</span>
-            <p class="top-box-text1">바코드 스캔</p>
-            <Btn btntype="solid" class="top-box-btn1">등록하기</Btn>
-        </div>
+    <p class="title-text1">제품 등록</p>
+    <div v-if="goodCount === 0">
+        <span class="material-symbols-rounded">barcode_scanner</span>
+        <p class="barcode-scan-text1">상품의 바코드를 스캔해주세요</p>
     </div>
-    <div class="menu-box">
-        <div class="menu-top-box">
-            <p style="margin-left: 50px;">상품코드</p>
-            <p style="margin-left: 85px;">사진</p>
-            <p style="margin-left: 210px;">제품명</p>
-            <p style="margin-left: 190px;">용량</p>
-            <p style="margin-left: 70px;">수량</p>
-            <p style="margin-left: 70px;">유통기한</p>
-            <p style="margin-left: 90px;">원가</p>
-            <p style="margin-left: 95px;">할인율</p>
-            <p style="margin-left: 85px;">판매가</p>
-            <p style="margin-left: 90px;">수정</p>
+
+    <div v-if="goodCount > 0">
+        <div class="menu-list-topbox1">
+            <div class="search">
+                <span class="material-symbols-rounded2">search</span>
+                <input
+                    class="search_input"
+                    type="text"
+                    placeholder="상품 코드 혹은 제품명 검색" />
+            </div>
+            <Btn btntype="ghostWhite" class="list_btn2">데이터 추가</Btn>
         </div>
-        <div class="menu-list">
-            <div class="menu-info" v-for="(item, idx) in goods">
-                <p style="flex-basis: 90px;">{{ item.code }}</p>
-                   <div class="menu-img"></div>
-                   <p style="flex-basis: 296px;">{{ item.title }}</p>
-                   <p style="flex-basis: 60px;">{{ item.capacity }}</p>
-                   <p style="flex-basis: 10px;">{{ item.quantity }}</p>
-                   <p style="flex-basis: 90px;">{{ item.day }}</p>
-                   <p style="flex-basis: 70px;">{{ item.price }}원</p>
-                   <p style="flex-basis: 70px;">{{ item.discount }}%</p>
-                   <p style="flex-basis: 70px;">{{ item.discountprice }}원</p>
-                <Btn btntype="LightSolid" class="list-btn1">수정</Btn>
+        <div class="menu-box">
+            <div class="menu-top-box">
+                <p style="margin-left: 90px;">상품코드</p>
+                <p style="margin-left: 360px;">제품명</p>
+                <p style="margin-left: 355px;">용량</p>
+                <p style="margin-left: 210px;">원가</p>
+                <p style="margin-left: 195px;">추가</p>
+            </div>
+            <div class="menu-list">
+                <div class="menu-info" v-for="(item, idx) in goods">
+                    <p style="flex-basis: 90px;">{{ item.code }}</p>
+                    <p style="flex-basis: 400px;">{{ item.title }}</p>
+                    <p style="flex-basis: 100px;">{{ item.capacity }}</p>
+                    <p style="flex-basis: 80px;">{{ item.price }}원</p>
+                    <Btn btntype="LightSolid" class="list-btn1">추가</Btn>
+                </div>
             </div>
         </div>
     </div>
+
+    <div class="scan-text-box1">
+            <p class="barcode-scan-text2">스캔이 되지 않는 경우</p>
+            <p class="barcode-scan-text3">문제 해결</p>
+            <p class="barcode-scan-text2">혹은</p>
+            <p class="barcode-scan-text3">바코드 입력</p>
+        </div>
  </div>
 </template>
 
 <script setup>
 import Sidebar from '../../common/main/sidebar/Sidebar.vue';
 import Btn from '../../common/components/Btn.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const goods = ref([
     {
@@ -79,39 +87,15 @@ const goods = ref([
       quantity: 1,
     },
 ]);
+
+const goodCount = computed(() => goods.value.length);
 </script>
 
 <style scoped>
-.top-box {
-    border-bottom: solid 1px #DDE5DB;
-}
-
-.top-box-items {
-    display: flex;
-    margin-left: 70px;
-    margin-top: 10px;
-    margin-bottom: 10px;
-}
-.top-box-text1 {
-    font-size: 28px;
-    font-weight: bold;
-    margin-left: 30px;
-    margin-top: 0px;
-    margin-bottom: 0px;
-}
-
-.material-symbols-rounded {
-  font-variation-settings: 'FILL' 0, 'wght' 500, 'GRAD' 0, 'opsz' 24;
-}
-.top-box-btn1 {
-    margin-left: auto;
-    padding: 10px 50px;
-    margin-right: 80px;
-}
-
 .menu-box {
     width: 90%;
     margin: 0 auto;
+    margin-top: 30px;
 }
 .menu-top-box {
     display: flex;
@@ -132,27 +116,109 @@ const goods = ref([
     flex-direction: column;
 }
 
+.menu-list-topbox1 {
+    display: flex;
+}
+
 .menu-info {
     display: flex;
     flex-grow: 1;
     justify-content: space-around;
-    margin-top: 10px;
-    padding-bottom: 10px;
     border-bottom: solid 1px #C1C9BF;
-}
-
-.menu-img {
-    width: 60px;
-    height: 60px;
-    background-color: #000;
+    font-size: 20px;
 }
 
 .list-btn1 {
     width: 56px;
-    height: 40px;
+    height: 35px;
     padding: 0px;
     padding-top: 12px;
     padding-left: 5px;
     padding-right: 5px;
+    margin-top: 10px;
+}
+
+.list_btn2 {
+    width: 100px;
+    height: 20px;
+    margin-left: auto;
+    margin-right: 100px;
+    margin-left: 900px;
+    margin-top: 150px;
+}
+
+.title-text1 {
+    font-size: 28px;
+    font-weight: bold;
+    float: left;
+    margin-left: 100px;
+    margin-top: 70px;
+}
+
+.material-symbols-rounded {
+  font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24;
+  font-size: 225px;
+  color: var(--ngray200);
+  margin-top: 350px;
+  margin-right: 190px;
+}
+
+.material-symbols-rounded2 {
+  font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24;
+}
+
+.barcode-scan-text1 {
+    font-size: 20px;
+    font-weight: 500;
+    color: var(--ngray300);
+    margin: 0 auto;
+    margin-top: 10px;
+}
+
+.scan-text-box1 {
+    display: flex;
+    margin-top: 350px;
+    justify-content: center;
+}
+
+.barcode-scan-text2 {
+    margin-right: 10px;
+    font-size: 20px;
+    color: var(--ngray300);
+    font-weight: 500;
+}
+
+.barcode-scan-text3 {
+    margin-right: 10px;
+    font-size: 20px;
+    font-weight: 500;
+}
+
+.search {
+  border-radius: 4px;
+  border: #ebf3e9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  opacity: 1;
+  background-color: #ebf3e9;
+  padding: 8px;
+  gap: 4px;
+  color: var(--ngray600);
+  width: 500px;
+  margin-top: 150px;
+  margin-left: -100px;
+
+}
+
+.search_input {
+  background-color: #ebf3e9;
+  font-family: Pretendard;
+  font-size: 16px;
+  border-radius: 4px;
+  border: none;
+  width: 90%;
+  outline: none;
 }
 </style>
